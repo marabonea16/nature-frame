094168a9c3ee52c95d9514c864a4aba585bfaeb9
marabonea16 <163434333+marabonea16@users.noreply.github.com>
2024-11-05 00:00:59 +0200

    page routing with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Shop from "./pages/Shop";
 function App() {
   return (
     <div>
-      <h1>
-        Nature Frames
-      </h1>
+     <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/sign-in" element={<SignIn/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/forgot-password" element={<ForgotPassword/>} />
+          <Route path="/shop" element={<Shop/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
