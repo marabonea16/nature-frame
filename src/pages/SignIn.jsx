1a40aca709406ad3c712bcab0eda3c85032877a5
PurzaVlad <vpurza@gmail.com>
2024-12-27 13:26:08 +0200

    complete sign in auth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import { AiFillEyeInvisible, AiFillEye} from "react-icons/ai";
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
@@ -9,11 +12,29 @@ export default function SignIn() {
     password: ''
   })
   const {email, password} = formData;
+  const navigate = useNavigate();
   function onChange(e){
     setFormData((prevState)=>({
       ...prevState,
       [e.target.id]: e.target.value,
     }))
+  }
+  async function onSubmit(e){
+    e.preventDefault()
+      try{
+        const auth = getAuth();
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        if(userCredential.user){
+          toast.success("You have successfully signed in.");
+          navigate("/");
+        }
+      } catch(error) {
+          if(error.code === "auth/user-not-found"){
+                toast.error("The email or password are  invalid."); 
+              } else{
+                toast.error("Something went wrong. Please try again.");
+              }
+      }
   }
   return (
     <section>
@@ -25,7 +46,7 @@ export default function SignIn() {
           />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20 '>
-          <form>
+          <form onSubmit={onSubmit}>
             <input 
               type="email" 
               id="email" 
