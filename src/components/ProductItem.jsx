2e55a0e4e7d7aca4374ad674cf22160ec96670b7
Mara Bonea <marabonea16@gmail.com>
2025-01-04 02:00:34 +0200

    delete and edit added to product component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
-
-export default function ProductItem({product, id}) {
+import { FaTrash } from 'react-icons/fa';
+import { MdEdit } from 'react-icons/md';
+export default function ProductItem({product, id, onEdit, onDelete}) {
   return (
     <li className='relative bg-white flex flex-col justify-between items-center
     shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow
@@ -10,18 +11,32 @@ export default function ProductItem({product, id}) {
             <img className="h-[170px] w-full object-cover hover:scale-105 
             transition-scale duration-200 ease-in" 
              loading='lazy'
-             src={products.imgUrls[0]}/>
+             src={product.imgUrls[0]}/>
             <div className='p-[10px] w-full'>
                 <p className='text-xl font-semibold m-0 truncate '>
-                    {products.name}
+                    {product.name}
                 </p>
                 <p className='text-[#195e24] mt-2 font-semibold'>
-                    {products.price
+                    {product.price
                     .toString()
                     .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </p>
             </div>
         </Link>
+        {onDelete && (
+            <FaTrash className='absolute bottom-2 right-2 
+            h-[14px] cursor-pointer text-red-700'
+            onClick={() => onDelete(id)}
+            />
+            
+        )}
+        {onEdit && (
+            <MdEdit className='absolute bottom-2 right-7 
+            h-[14px] cursor-pointer text-green-800'
+            onClick={() => onEdit(id)}
+            />
+            
+        )}
     </li>
   )
 }
