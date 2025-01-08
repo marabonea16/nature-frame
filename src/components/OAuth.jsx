33a950f714b78a6cbf41be856495bfe51f3c0a44
Mara Bonea <marabonea16@gmail.com>
2025-01-08 16:30:34 +0200

    remove fb auth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { FcGoogle } from "react-icons/fc";
-import { FaFacebookF } from "react-icons/fa";
-import { FacebookAuthProvider, getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { toast } from 'react-toastify';
 import { db } from "../firebase.js";
 import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
@@ -28,42 +27,11 @@ export default function OAuth() {
       toast.error("Could not authenticate with Google. Please try again.")
     }
   }
-  async function onFacebookClick(){
-    try {
-      const auth = getAuth();
-      const provider = new FacebookAuthProvider();
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      console.log(user);
-      const docRef = doc(db, "users", user.uid);
-      const docSnap = await getDoc(docRef);
-      if(!docSnap.exists()){
-        await setDoc(docRef, {
-          name: user.displayName,
-          email: user.email,
-          timestamp: serverTimestamp(), 
-        });
-      } 
-      navigate("/");
-    } catch (error) {
-      toast.error("Could not authenticate with Facebook. Please try again.")
-    }
-  }
+  
+  
   return (
     <div className='justify-center space-y-6'>
-      <button 
-        type = "button"
-        onClick={onFacebookClick}
-        className='flex items-center justify-center w-full
-        bg-blue-600 text-white px-7 py-3 uppercase
-        text-sm font-medium hover:bg-blue-700 active:bg-blue-800
-        shadow-md hover:shadow-lg active:shadow-lg transition 
-        duration-10 ease-in-out rounded'>
-        <div className='flex items-center justify-center bg-white rounded-full p-1 mr-2'>
-          <FaFacebookF className='text-blue-600 text-xl'/>
-        </div>
-        Continue with Facebook
-      </button>
+      
       <button
          type = "button"
          onClick={onGoogleClick}
